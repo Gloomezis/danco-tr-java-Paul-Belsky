627a6a5270de7eb49813aa8938f6e9dfa146172b
Gloomezis <Gloomezis.ru@mail.ru>
2016-03-10 16:38:40 +0300

    Exercise 1,2 TASK 9

---
+++ DML.sql
@@ -0,0 +1,42 @@
+
+INSERT INTO product VALUES ('A', 'Laptop1', 'Laptop');
+INSERT INTO product VALUES ('A', 'Printer1', 'Printer');
+INSERT INTO product VALUES ('B', 'Printer2', 'Printer');
+INSERT INTO product VALUES ('B', 'Pc1', 'PC');
+INSERT INTO product VALUES ('B', 'Pc2', 'PC');
+INSERT INTO product VALUES ('C', 'Laptop2', 'Laptop');
+INSERT INTO product VALUES ('D', 'Pc3', 'PC' );
+INSERT INTO product VALUES ('D', 'Printer3', 'Printer');
+INSERT INTO product VALUES ('D', 'Laptop3', 'Laptop');
+INSERT INTO product VALUES ('D', 'Laptop4', 'Laptop');
+INSERT INTO product VALUES ('D', 'Printer4', 'Printer');
+INSERT INTO product VALUES ('D', 'Pc4', 'PC');
+INSERT INTO product VALUES ('D', 'Laptop5', 'Laptop');
+INSERT INTO product VALUES ('D', 'Printer5', 'Printer');
+INSERT INTO product VALUES ('D', 'Pc5', 'PC');
+INSERT INTO product VALUES ('D', 'Pc6', 'PC');
+INSERT INTO product VALUES ('A', 'Laptop6', 'Laptop');
+
+
+
+INSERT INTO laptop VALUES (0, 'Laptop1', 500, 12, 100, 356, 14);
+INSERT INTO laptop VALUES (1, 'Laptop2', 200, 24, 200, 485, 15);
+INSERT INTO laptop VALUES (2, 'Laptop3', 1600, 36, 300, 516,17);
+INSERT INTO laptop VALUES (3, 'Laptop4', 800, 48, 400, 290, 15);
+INSERT INTO laptop VALUES (4, 'Laptop5', 300, 60, 500, 860, 19);
+INSERT INTO laptop VALUES (5, 'Laptop6', 1200, 72, 500, 1100, 14);
+
+
+INSERT INTO printer VALUES (0, 'Printer1', 'y', 'Laz', 115);
+INSERT INTO printer VALUES (1, 'Printer2', 'n', 'Jet', 98);
+INSERT INTO printer VALUES (2, 'Printer3', 'n', 'Mat', 135);
+INSERT INTO printer VALUES (3, 'Printer4', 'y', 'Mat', 99);
+INSERT INTO printer VALUES (4, 'Printer5', 'n', 'Laz', 86);
+
+
+INSERT INTO pc VALUES (0, 'Pc1', 290, 36, 100, '4x', 400);
+INSERT INTO pc VALUES (1, 'Pc2', 500, 24, 300, '8x', 452);
+INSERT INTO pc VALUES (2, 'Pc3', 700, 60, 200, '16x', 780);
+INSERT INTO pc VALUES (3, 'Pc4', 440, 38, 50, '12x', 560);
+INSERT INTO pc VALUES (4, 'Pc5', 1600, 12, 200, '32x', 490);
+INSERT INTO pc VALUES (5, 'Pc6', 1600, 12, 200, '24x', 599);
---
+++ SQL QUERY.sql
@@ -0,0 +1,144 @@
+1. SELECT model,speed, hd FROM pc
+WHERE price<500;
+
+2. SELECT DISTINCT maker FROM product 
+WHERE  type ='printer';
+
+3. SELECT model, hd,screen FROM laptop
+Where price>1000;
+
+4. SELECT * FROM printer
+Where color='y';
+
+5. SELECT model, speed, hd FROM pc WHERE price < 600.0 and cd='12õ' OR cd = '24x';
+
+6. SELECT product.maker ,laptop.speed FROM product
+ INNER JOIN laptop ON product.model=laptop.model
+ WHERE laptop.hd>=10;
+7.
+ (SELECT laptop.model , price   FROM laptop
+ INNER JOIN  product ON laptop.model=product.model
+ WHERE  product.maker = 'Fourth')
+ UNION
+ (SELECT pc.model , price   FROM pc
+ INNER JOIN product ON pc.model=product.model
+ WHERE  product.maker = 'Fourth')
+ UNION
+ (SELECT printer.model , price   FROM printer
+ INNER JOIN product ON printer.model=product.model
+ WHERE  product.maker = 'Fourth');
+ 
+8.TODO
+SELECT DISTINCT maker FROM product
+where type = 'pc'
+NOT IN
+SELECT DISTINCT maker
+from product
+WHERE
+
+9.
+SELECT DISTINCT maker FROM product
+INNER JOIN pc ON product.model=pc.model
+WHERE pc.speed>=35;
+
+10. 
+SELECT model,price  FROM printer
+WHERE price =(SELECT MAX(price) FROM printer);
+
+11.
+SELECT AVG(speed) FROM pc;
+
+12.
+SELECT AVG(speed) FROM laptop
+WHERE price>1000;
+
+13.
+SELECT AVG(pc.speed) from pc
+inner join product on
+product.model=pc.model
+where product.maker = 'A';
+
+14
+SELECT speed, AVG(price) FROM pc
+GROUP BY speed;
+
+15.
+SELECT hd FROM pc
+GROUP BY hd
+HAVING COUNT(model)>1;
+
+16.
+SELECT DISTINCT one.model AS model , two.model AS model, one.speed,one.ram FROM pc AS one
+INNER JOIN pc AS two ON
+one.speed=two.speed AND one.ram=two.ram AND one.model>two.model;
+
+17.
+SELECT DISTINCT pd.type, lp.model, lp.speed FROM laptop AS lp
+INNER JOIN product AS pd ON
+lp.model=pd.model
+WHERE lp.speed<(SELECT MIN(pc.speed) FROM pc);
+
+18. 
+SELECT pd.maker, pt.price FROM printer AS pt
+INNER JOIN product AS pd ON
+pt.model=pd.model
+WHERE pt.price = (SELECT MIN(price) FROM printer  WHERE color='y' ) AND pt.color='y';
+
+19.
+SELECT pd.maker, AVG(lp.screen) AS AVG_SC from laptop AS lp
+INNER JOIN product AS pd ON
+pd.model=lp.model
+GROUP BY pd.maker;
+
+20.
+SELECT pd.maker, count(pc.model) AS pc_models from pc 
+INNER JOIN product AS pd ON
+pd.model=pc.model
+GROUP BY maker
+HAVING count(pc.model)>3;
+
+// ALTERNATIVE
+
+SELECT maker, count(model) AS pc_models from product 
+WHERE type = 'pc'
+GROUP BY maker
+HAVING COUNT(model)>3;
+
+21.
+SELECT pd.maker, MAX(pc.price) AS Max_price from pc
+INNER JOIN product AS pd ON
+pd.model=pc.model
+GROUP BY pd.maker;
+
+22.
+SELECT speed , AVG(price) FROM pc
+WHERE speed>600
+GROUP BY speed;
+
+23.
+SELECT DISTINCT maker FROM product
+INNER JOIN pc ON
+product.model=pc.model
+WHERE pc.speed>=750 AND maker IN
+(SELECT maker FROM product
+INNER JOIN laptop ON
+product.model=laptop.model
+WHERE  laptop.speed>=750);
+
+24. 
+SELECT model FROM
+(SELECT model,price FROM pc
+UNION
+SELECT model,price FROM laptop
+UNION
+SELECT model,price FROM printer) all_max_price
+WHERE price=(SELECT MAX(price) FROM (
+SELECT price FROM pc
+UNION
+SELECT price FROM laptop
+UNION
+SELECT price FROM printer) all_price
+);
+//1 Getting all price. 2. Finding max price 3. Searching max price in all model separeted
+
+
